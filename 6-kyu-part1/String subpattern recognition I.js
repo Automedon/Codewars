67828b76ce0eab4b3abcfd402f1a8f2dc9e2367e
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-18 08:26:39 +0700

    Create String subpattern recognition I.js

---
+++ 6-kyu-part1/String subpattern recognition I.js
@@ -0,0 +1,18 @@
+/*
+Description:
+In this kata you need to build a function to return either true/True or false/False if a string can be seen as the repetition of a simpler/shorter subpattern or not.
+
+For example:
+
+hasSubpattern("a") === false; //no repeated pattern
+hasSubpattern("aaaa") === true; //created repeating "a"
+hasSubpattern("abcd") === false; //no repeated pattern
+hasSubpattern("abababab") === true; //created repeating "ab"
+hasSubpattern("ababababa") === false; //cannot be entirely reproduced repeating a pattern
+Strings will never be empty and can be composed of any character (just consider upper- and lowercase letters as different entities) and can be pretty long (keep an eye on performances!).
+
+If you liked it, go for the next kata of the series!
+*/
+function hasSubpattern(string){
+  return string.match(/^(.*)\1+$/)!==null;
+}
