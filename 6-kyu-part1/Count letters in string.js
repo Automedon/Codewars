8cac5d3014b083a9a449cae5d2e7a2c88efe4a5e
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-18 08:21:32 +0700

    Create Count letters in string.js

---
+++ 6-kyu-part1/Count letters in string.js
@@ -0,0 +1,10 @@
+/*
+In this kata, you've to count lowercase letters in a given string and return the letter count in a hash with 'letter' as key and count as 'value'. The key must be 'symbol' instead of string in Ruby and 'char' instead of string in Crystal.
+
+Example:
+
+letter_count('arithmetics') #=> {"a": 1, "c": 1, "e": 1, "h": 1, "i": 2, "m": 1, "r": 1, "s": 1, "t": 2}
+*/
+function letterCount(s){
+  return s.split``.reduce((a,b)=>(a[b]=a[b]+1||1,a),{})
+}
