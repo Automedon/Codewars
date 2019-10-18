1005e03ce1a2fef225e30ed34b33c56e91b03cb5
Valefar React Developer <inzhener.intellect@gmail.com>
2019-10-18 08:36:23 +0700

    Create A functional deck of cards.....js

---
+++ 6-kyu-part1/A functional deck of cards.....js
@@ -0,0 +1,23 @@
+/*
+Description:
+Your task should you chose to accept...
+Build a deck of playing cards by generating 52 strings representing cards. There are no jokers.
+
+Each card string will have the format of:
+
+"ace of hearts"
+"ace of spades"
+"ace of diamonds"
+"ace of clubs"
+And will consist of the following ranks:
+
+ace two three four five
+six seven eight nine ten
+jack queen king
+They do not need to be in order.
+
+Additional constraints
+1 line only!
+buildDeck is a variable holding your deck. It is not a function.
+*/
+let buildDeck = `ace two three four five six seven eight nine ten jack queen king `.replace(/\w+\s?/g,v=>`${v}of hearts|${v}of spades|${v}of diamonds|${v}of clubs|`).slice(0,-1).split`|`
