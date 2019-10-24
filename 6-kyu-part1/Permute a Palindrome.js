4e78e28127e94d7fc8c5e6cbba01234384c40388
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2019-10-24 10:29:43 +0700

    Create Permute a Palindrome.js

---
+++ 6-kyu-part1/Permute a Palindrome.js
@@ -0,0 +1,23 @@
+/*
+Description:
+Write a function that will check whether the permutation of an input string is a palindrome. Bonus points for a solution that is efficient and/or that uses only built-in language functions. Deem yourself brilliant if you can come up with a version that does not use any function whatsoever.
+
+Example
+madam -> True
+adamm -> True
+junk -> False
+
+Hint
+The brute force approach would be to generate all the permutations of the string and check each one of them whether it is a palindrome. However, an optimized approach will not require this at all.
+*/
+function permuteAPalindrome (input) { 
+  let count = 0
+  const dict = input.split``.reduce((a,b)=>(a[b]=a[b]?a[b]+1:1,a),{})
+  for (let i in dict){
+    if (dict[i]%2!==0){
+      count++
+    }
+    if (count===2) return false
+  }
+  return true;
+}
