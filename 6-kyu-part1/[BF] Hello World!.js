3569501cc506e0f34fddea9cdf642244adb63687
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2019-10-31 10:10:34 +0700

    Create [BF] Hello World!.js

---
+++ 6-kyu-part1/[BF] Hello World!.js
@@ -0,0 +1,5 @@
+/*
+Description:
+Write program in BrainFuck that returns "Hello World!"
+*/
+let prog = '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.';
