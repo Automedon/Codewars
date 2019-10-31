d5870643c290ed3caa25b05889c2402ccc1ce05e
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2019-10-31 13:16:01 +0700

    Create TV channels.js

---
+++ 6-kyu-part1/TV channels.js
@@ -0,0 +1,17 @@
+/*
+Description:
+Program channels into your TV's memory. An array with channels (strings) will be passed as an argument to the function redarr(). Sort the channels in an alphabetical order, remove duplicates and, finally, return an object where each channel (object's value) is assigned to a whole number (objects's key), starting with 0.
+
+Examples:
+
+var arr = ["BBC1", "BBC2", "MTV"];
+
+redarr(arr) // returns {"0":"BBC1", "1":"BBC2", "2":"MTV"}
+
+var arr = ["BBC1", "BBC1", "BBC2", "MTV"];
+
+redarr(arr) // returns {"0":"BBC1", "1":"BBC2", "2":"MTV"}
+*/
+function redarr(arr) {
+  return [...new Set(arr.sort())].reduce((a,b,i)=>(a[i]=b,a),{})
+}
