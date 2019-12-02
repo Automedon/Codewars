1e72888e86ccc6f657b91f0d8d0b918d9cb5c6b0
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2019-12-02 10:52:29 +0700

    Create JS is Weird: Return a.js

---
+++ 6-kyu-part1/JS is Weird: Return a.js
@@ -0,0 +1,12 @@
+/*
+There are only two tests. Make the function return 'a' as a string, and don't use any banned characters.
+
+You can only use the following characters:
+
++ ( ) [ ] ! x =
+
+x= is the initial solution and should be used only to define x. x is preloaded as a variable.
+
+All other characters (including spaces) are banned.
+*/
+x=(![]+[])[+!![]]
