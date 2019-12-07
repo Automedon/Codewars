bbcecb3df66b5d3b953fd89a750ce3947249b2bd
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2019-12-07 20:52:59 +0700

    Create 5 without numbers !!.js

---
+++ 8-kyu/5 without numbers !!.js
@@ -0,0 +1,11 @@
+/*
+Description:
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+*/
+function unusualFive() {
+  return ['a','a','a','a','a'].length
+}
