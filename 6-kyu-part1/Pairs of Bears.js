9b1f1fcf2afb66d34d0de60a98d7451b9a8e902c
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2019-12-09 10:25:07 +0700

    Create Pairs of Bears.js

---
+++ 6-kyu-part1/Pairs of Bears.js
@@ -0,0 +1,22 @@
+/*
+Description:
+In order to prove it's success and gain funding, the wilderness zoo needs to prove to environmentalists that it has x number of mating pairs of bears.
+
+You must check within string (s) to fid all of the mating pairs, and return a string containing only them. Line them up for inspection.
+
+Rules: Bears are either 'B' (male) or '8' (female), Bears must be together in male/female pairs 'B8' or '8B', Mating pairs must involve two distinct bears each ('B8B' may look fun, but does not count as two pairs).
+
+Return an array containing a string of only the mating pairs available. e.g:
+
+'EvHB8KN8ik8BiyxfeyKBmiCMj' ---> 'B88B' (empty string if there are no pairs)
+
+and true if the number is more than or equal to x, false if not:
+
+(6, 'EvHB8KN8ik8BiyxfeyKBmiCMj') ---> ['B88B', false];
+
+x will always be a positive integer, and s will never be empty
+*/
+function bears(x, s){
+  let match = s.match(/8B|B8/g)||['']
+  return [match.join``,match.length>=x]
+}
