e4479fd76354feead034032fdd8ac9987c3c9428
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-01-22 20:46:56 +0700

    Create True Addition.js

---
+++ 6-kyu-part1/True Addition.js
@@ -0,0 +1,20 @@
+/*
+Description:
+Given a target number (n), return that number.
+
+You are not allowed to use any numbers (or string representations of numbers).
+
+You are not allowed to declare any variables other than the one given (x).
+
+You are not allowed to set variable x to equal n, either directly or by using 'arguments'.
+
+You are not allowed to use 'arguments' to reference n.
+
+You may not use 'Array'.
+
+You may not return variable n. Your function must return variable x.
+*/
+function count(n){
+  var x = true;
+  return x*n;
+}
