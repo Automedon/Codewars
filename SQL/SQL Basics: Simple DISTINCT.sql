a76e1a21db1610e8e8ee4c5ff4d6844a8789d0c5
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-03-29 22:14:48 +0700

    Create  SQL Basics: Simple DISTINCT.sql

---
+++ SQL/SQL Basics: Simple DISTINCT.sql
@@ -0,0 +1,12 @@
+/*
+Description:
+For this challenge you need to create a simple DISTINCT statement, you want to find all the unique ages.
+
+people table schema
+id
+name
+age
+select table schema
+age (distinct)
+*/
+SELECT DISTINCT age FROM people;
