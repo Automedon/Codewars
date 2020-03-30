2ab91a56aefa58b25b546a9836f64b9d3619c4ea
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-03-30 20:12:10 +0700

    Create 1. Find all active students.sql

---
+++ SQL/1. Find all active students.sql
@@ -0,0 +1,12 @@
+/*
+Description:
+Create a simple SELECT query to display student information of all ACTIVE students.
+
+TABLE STRUCTURE:
+
+students
+Id	FirstName	LastName	IsActive
+
+Note: IsActive (true or false)
+*/
+SELECT * FROM students WHERE isActive;
