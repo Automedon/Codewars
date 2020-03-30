93d6c35913b3f4e5dd3ed985919349607ab3c57b
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-03-30 20:15:13 +0700

    Create SQL Basics: Mod.sql

---
+++ SQL/SQL Basics: Mod.sql
@@ -0,0 +1,12 @@
+/*
+Description:
+Given the following table 'decimals':
+
+** decimals table schema **
+
+id
+number1
+number2
+Return a table with one column (mod) which is the output of number1 modulus number2.
+*/
+SELECT MOD (number1,number2) AS mod FROM decimals;
