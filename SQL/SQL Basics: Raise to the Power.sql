b12941a6f0f7de416d75fcef78921d7e97a52116
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-04-03 21:38:52 +0700

    Create  SQL Basics: Raise to the Power.sql

---
+++ SQL/SQL Basics: Raise to the Power.sql
@@ -0,0 +1,12 @@
+/*
+Description:
+Given the following table 'decimals':
+
+** decimals table schema **
+
+id
+number1
+number2
+Return a table with one column (result) which is the output of number1 raised to the power of number2.
+*/
+SELECT number1^number2 AS result FROM decimals;
