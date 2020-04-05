7ca8d36e67acef479628579dfb584e4688adc133
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-04-05 15:42:10 +0700

    Create BASICS: Length based SELECT with LIKE.sql

---
+++ SQL/BASICS: Length based SELECT with LIKE.sql
@@ -0,0 +1,15 @@
+/*
+Description:
+You will need to create SELECT statement in conjunction with LIKE.
+
+Please list people which have first_name with at least 6 character long
+
+names table schema
+id
+first_name
+last_name
+results table schema
+first_name
+last_name
+*/
+SELECT first_name, last_name FROM names WHERE first_name LIKE '______%';
