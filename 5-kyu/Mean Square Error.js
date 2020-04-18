47bfb2d9a9c5aa6245b36a10599b69e3ec6705a4
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-04-18 16:49:50 +0700

    Create  Mean Square Error.js

---
+++ 5-kyu/Mean Square Error.js
@@ -0,0 +1,16 @@
+/*
+Description:
+Complete the function that
+
+accepts two integer arrays of equal length
+compares the value each member in one array to the corresponding member in the other
+squares the absolute value difference between those two values
+and returns the average of those squared absolute value difference between each member pair.
+Examples
+[1, 2, 3], [4, 5, 6]              -->   9   because (9 + 9 + 9) / 3
+[10, 20, 10, 2], [10, 25, 5, -2]  -->  16.5 because (0 + 25 + 25 + 16) / 4
+[-1, 0], [0, -1]                  -->   1   because (1 + 1) / 2
+*/
+const solution=(f,s)=>{
+  return f.map((v,i)=>Math.pow(Math.abs(s[i]-v),2)).reduce((a,b)=>a+b,0)/f.length
+}
