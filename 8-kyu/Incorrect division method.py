21ba0b96f554653c8fef68190ff8f058efc7bdad
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-04-28 20:05:33 +0700

    Create Incorrect division method.py

---
+++ 8-kyu/Incorrect division method.py
@@ -0,0 +1,6 @@
+"""
+Description:
+This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+"""
+def divide_numbers(x,y):
+    return x / y
