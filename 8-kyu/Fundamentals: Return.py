52059ce8ae2f58843eeeafab6ecedbc3bd52d4c6
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-04-28 21:58:43 +0700

    Create Fundamentals: Return.py

---
+++ 8-kyu/Fundamentals: Return.py
@@ -0,0 +1,32 @@
+"""
+Description:
+Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+Please use the following function names:
+
+addition = add
+
+multiply = multiply
+
+division = divide (both integer and float divisions are accepted)
+
+modulus = mod
+
+exponential = exponent
+
+subtraction = subt
+
+Note: All math operations will be: a (operation) b
+"""
+def add(a,b):
+    return a+b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+def mod(a,b):
+    return a%b
+def exponent(a,b):
+    return a**b
+def subt(a,b):
+    return a-b
