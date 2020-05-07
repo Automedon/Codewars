3f703c4d6017c99a1381338676ffe77257fd6373
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-05-07 22:35:08 +0700

    Create  Happy Birthday, Darling!.js

---
+++ 7-kyu-part2/Happy Birthday, Darling!.js
@@ -0,0 +1,17 @@
+/*
+Description:
+As you may know, once some people pass their teens, they jokingly only celebrate their 20th or 21st birthday, forever. With some maths skills, that's totally possible - you only need to select the correct number base!
+
+For example, if they turn 32, that's exactly 20 - in base 16... Already 39? That's just 21, in base 19!
+
+Your task is to translate the given age to the much desired 20 (or 21) years, and indicate the number base, in the format specified below.
+
+Note: input will be always > 21
+
+Examples:
+32  -->  "32? That's just 20, in base 16!"
+39  -->  "39? That's just 21, in base 19!"
+*/
+function womensAge(n) {
+  return `${n}? That's just ${20+n%2}, in base ${Math.floor(n/2)}!`;
+}
