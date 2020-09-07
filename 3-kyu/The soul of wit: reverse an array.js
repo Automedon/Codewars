629d1f3eef842ab4d4e7128bfd43de5e188db603
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-09-07 20:06:55 +0700

    Create  The soul of wit: reverse an array.js

---
+++ 3-kyu/The soul of wit: reverse an array.js
@@ -0,0 +1,13 @@
+/*
+No time for stories. Reverse an array, return the result. Do whatever you want with the original array. Don't use Array.prototype.reverse.
+
+You have 30 bytes to spare.
+
+Example: [1, 2, 3] → [3, 2, 1]
+
+And this time you won't be able to do the thing from that kata.
+
+require isn't allowed as well.
+*/
+
+reverse=a=>[...a].map(a.pop,a)
