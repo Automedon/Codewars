5dcb865facdfe69a092cfeb6a0bb30a1f6da251b
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-10-17 08:40:18 +0700

    Create Most frequently used words in a text.js

---
+++ 4-kyu/Most frequently used words in a text.js
@@ -0,0 +1,39 @@
+/*
+Description:
+Write a function that, given a string of text (possibly with punctuation and line-breaks), returns an array of the top-3 most occurring words, in descending order of the number of occurrences.
+
+Assumptions:
+A word is a string of letters (A to Z) optionally containing one or more apostrophes (') in ASCII. (No need to handle fancy punctuation.)
+Matches should be case-insensitive, and the words in the result should be lowercased.
+Ties may be broken arbitrarily.
+If a text contains fewer than three unique words, then either the top-2 or top-1 words should be returned, or an empty array if a text contains no words.
+Examples:
+top_3_words("In a village of La Mancha, the name of which I have no desire to call to
+mind, there lived not long since one of those gentlemen that keep a lance
+in the lance-rack, an old buckler, a lean hack, and a greyhound for
+coursing. An olla of rather more beef than mutton, a salad on most
+nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+on Sundays, made away with three-quarters of his income.")
+# => ["a", "of", "on"]
+
+top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e")
+# => ["e", "ddd", "aa"]
+
+top_3_words("  //wont won't won't")
+# => ["won't", "wont"]
+Bonus points (not really, but just for fun):
+Avoid creating an array whose memory footprint is roughly as big as the input text.
+Avoid sorting the entire array of unique words.
+*/
+function topThreeWords(text) {
+  const arr = []
+  text = text.toLowerCase().split` `.map(v=>{
+    if (v===`'`) return ''
+    return v.replace(/[^'a-z]/gi,'')
+  }).filter(v=>v)
+  const obj = text.reduce((a,b)=>(a[b]=a[b]?a[b]+1:1,a),{})
+  for (let i in obj){
+    arr.push([i,obj[i]])
+  }
+  return arr.sort((a,b)=>b[1]-a[1]).slice(0,3).map(v=>v[0])
+}
