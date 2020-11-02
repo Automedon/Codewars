66d4b1dbbda776b5416d21ec978060a52d9ffd7a
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-11-02 19:35:25 +0700

    Create Let's Recycle!.js

---
+++ 6-kyu-part1/Let's Recycle!.js
@@ -0,0 +1,38 @@
+/*
+Description:
+Task
+You will be given a list of objects. Each object has type, material, and possibly secondMaterial. The existing materials are: paper, glass, organic, and plastic.
+
+Your job is to sort these objects across the 4 recycling bins according to their material (and secondMaterial if it's present), by listing the type's of objects that should go into those bins.
+
+Notes
+The bins should come in the same order as the materials listed above
+All bins should be listed in the output, even if some of them are empty
+If an object is made of two materials, its type should be listed in both of the respective bins
+The order of the type's in each bin should be the same as the order of their respective objects was in the input list
+Example
+input = [
+  {"type": "rotten apples", "material": "organic"},
+  {"type": "out of date yogurt", "material": "organic", "secondMaterial": "plastic"},
+  {"type": "wine bottle", "material": "glass", "secondMaterial": "paper"},
+  {"type": "amazon box", "material": "paper"},
+  {"type": "beer bottle", "material": "glass", "secondMaterial": "paper"}
+]
+
+output = [
+  ["wine bottle", "amazon box", "beer bottle"],
+  ["wine bottle", "beer bottle"],
+  ["rotten apples", "out of date yogurt"],
+  ["out of date yogurt"]
+]
+*/
+function recycle(array) {
+  const obj = {'paper': [], 'glass': [], 'organic':[], 'plastic': []};
+  for (let i of array) {
+    obj[i.material].push(i.type);
+    if (i.secondMaterial !== undefined){
+      obj[i.secondMaterial].push(i.type)
+    }
+  }
+  return [obj['paper'], obj['glass'], obj['organic'], obj['plastic']];
+}
