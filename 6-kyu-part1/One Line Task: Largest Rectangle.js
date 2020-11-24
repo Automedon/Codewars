32136c038b6192d15e4318ea3aca59a63555c54e
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2020-11-24 07:18:37 +0700

    Create  One Line Task: Largest Rectangle.js

---
+++ 6-kyu-part1/One Line Task: Largest Rectangle.js
@@ -0,0 +1,16 @@
+/*
+Description:
+Task
+Among all rectangles that have a perimeter of p, find the dimensions of the one whose area is the largest. Returns its height and width as [height,width].
+
+Code Limit
+Strictly less than:
+
+26 characters in JavaScript
+47 characters in PHP
+Example
+For p = 100, the output should be [25,25].
+
+For p = 10, the output should be [2.5,2.5].
+*/
+rectanglePair=p=>[p/=4,p]
