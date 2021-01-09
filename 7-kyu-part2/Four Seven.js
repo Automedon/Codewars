6cc568bde2b905ec964264046f245e2f6495c5a1
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2021-01-09 21:30:40 +0700

    Create Four Seven.js

---
+++ 7-kyu-part2/Four Seven.js
@@ -0,0 +1,12 @@
+/*Description:
+Four Seven
+Simple kata, simple rules: your function should accept the inputs 4 and 7. If 4 is entered, the function should return 7. If 7 is entered, the function should return 4. Anything else entered as input should return 0. There's only one catch, your function cannot include if statements, switch statements, or the ternary operator (or the eval function due to the fact that you can get around the if requirement using it).
+
+There are some very simple ways of answering this problem, but I encourage you to try and be as creative as possible.
+
+Good Luck!
+*/
+
+function fourSeven(n){
+  return {7:4, 4:7}[n] || 0;
+}
