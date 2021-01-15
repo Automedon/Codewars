99a2c0da97165ac504a7bc8f41512504960994e8
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2021-01-15 21:58:42 +0700

    Create Shorten Me: Mad Mathematician's Algorithm IV (Division).js

---
+++ 7-kyu-part2/Shorten Me: Mad Mathematician's Algorithm IV (Division).js
@@ -0,0 +1,18 @@
+/*
+Description:
+Shorten Me: Mad Mathematician's Algorithm IV (Division)
+Task
+You have to write a madDiv(a,b) function returning a value of a / b without / in your code.
+
+Coding Limitation:
+Less than 30 characters
+Input:
+a and b : integer ( 0-10000 )
+Note: Only first 10 decimal places will be evaluated.
+|||| |---|---:| |Previous: Part III (Multiplication)|Next: Part V (Modulus)|
+
+|| |:---:| |Shorten Me: Mad Mathematician's Algorithm Series| |Mad Mathematician Collection|
+
+
+*/
+madDiv=(a,b)=>a*b**-1
