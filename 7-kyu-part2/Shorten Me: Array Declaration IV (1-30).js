29d11ef4a12e7c6f850deba83cd7b00cdc2d7fbb
Valefar React Developer <42299107+Automedon@users.noreply.github.com>
2021-01-16 22:30:00 +0700

    Create Shorten Me: Array Declaration IV (1-30).js

---
+++ 7-kyu-part2/Shorten Me: Array Declaration IV (1-30).js
@@ -0,0 +1,17 @@
+/*
+Description:
+Shorten Me: Array Declaration IV (1-30)
+Task
+You have to write an array() function returning an array object with elements as follow:
+
+[ 1,   2,  3,  4,  5,  6,  7,  8,  9, 10,
+  11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30 ]
+Coding Limitation:
+Less than 40 characters
+
+|||| |---|---:| |Previous: Part III (Fibonacci Last Digit)|Next: Part V (Weekday & Weekend)|
+
+|| |:---:| |Shorten Me: Array Declaration Series|
+*/
+array=_=>[...Array(30)].map((x,i)=>i+1)
