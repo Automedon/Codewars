9a72dcfc69394b82800c5dc1540576476568c772
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2021-09-03 11:09:21 +0700

    Create Strange mathematics.js

---
+++ 7-kyu-part2/Strange mathematics.js
@@ -0,0 +1,26 @@
+/*
+Description:
+In a certain kingdom, strange mathematics is taught at school. Its main difference from ordinary mathematics is that the numbers in it are not ordered in ascending order, but lexicographically, as in a dictionary (first by the first digit, then, if the first digit is equal, by the second, and so on). In addition, we do not consider an infinite set of natural numbers, but only the first n numbers.
+
+So, for example, if n = 11, then the numbers in strange mathematics are ordered as follows:
+
+1, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9.
+
+Help your students to learn this science: write a function that receives two integer numbers: n (total amount of numbers in strange mathematics) and k (number from sequence) and returns the location of a given number k in the order defined in strange mathematics.
+
+For example, if n = 11 and k = 2, the function should return 4 as the answer.
+
+Input: 1 <= n <= 100 000 , 1 <= k <= n.
+
+Output: position of the number k in sequence of the first n natural numbers in lexicographic order. Numbering starts with 1.
+
+Examples:
+strangeMath(11, 2) === 4
+strangeMath(15, 5) === 11
+strangeMath(15, 15) === 7
+*/
+
+function strangeMath(n, k){
+  const arr = Array.from(Array(n),(_,i)=>i+1).sort((a,b)=>a.toString().localeCompare(b.toString()))
+  return arr.indexOf(k) + 1
+}
