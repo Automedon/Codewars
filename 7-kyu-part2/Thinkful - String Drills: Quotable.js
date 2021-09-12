1d80374d05dc81a9b8ceec6ca14ad311954d7125
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2021-09-12 14:37:41 +0700

    Create Thinkful - String Drills: Quotable.js

---
+++ 7-kyu-part2/Thinkful - String Drills: Quotable.js
@@ -0,0 +1,16 @@
+/*
+Description:
+This function should take two string parameters: a person's name (name) and a quote of theirs (quote), and return a string attributing the quote to the person in the following format:
+
+'[name] said: "[quote]"'
+For example, if name is 'Grae' and 'quote' is 'Practice makes perfect' then your function should return the string
+
+'Grae said: "Practice makes perfect"'
+Unfortunately, something is wrong with the instructions in the function body. Your job is to fix it so the function returns correctly formatted quotes.
+
+Click the "Train" button to get started, and be careful with your quotation marks.
+*/
+
+function quotable(name, quote){
+  return `${name} said: "${quote}"`
+}
