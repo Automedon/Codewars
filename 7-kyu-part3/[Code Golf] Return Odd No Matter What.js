7d876b1cafe46006413374dbd65269fc98668706
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2021-10-05 09:50:23 +0700

    Create [Code Golf] Return Odd No Matter What.js

---
+++ 7-kyu-part3/[Code Golf] Return Odd No Matter What.js
@@ -0,0 +1,20 @@
+/*
+Description:
+Given the integer n return odd numbers as they are, but subtract 1 from even numbers.
+
+Note:
+
+Your solution should be 22 or less characters long.
+
+Examples
+Input  = 2
+Output = 1
+
+Input  = 13
+Output = 13
+
+Input  = 46
+Output = 45
+*/
+
+alwaysOdd=n=>n%2?n:n-1
