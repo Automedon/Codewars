c3bfe35a4a9ec0ddfe808a43d51a26bbfd53c028
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2021-10-13 09:09:05 +0700

    Create Counter of neighbor ones.js

---
+++ 7-kyu-part3/Counter of neighbor ones.js
@@ -0,0 +1,29 @@
+/*
+Description:
+Task
+Tranform of input array of zeros and ones to array in which counts number of continuous ones. If there is none, return an empty array
+
+Example
+[1, 1, 1, 0, 1] -> [3,1]
+[1, 1, 1, 1, 1] -> [5]
+[0, 0, 0, 0, 0] -> []
+*/
+
+function onesCounter(input) {
+  const arr = [];
+  let count = 0
+  for(let i =0;i<input.length;i++){
+    if (input[i]){
+      count+=1
+    } else {
+        if(count){
+          arr.push(count)
+          }
+        count = 0
+    }
+  }
+  if(count){
+    arr.push(count)
+  }
+  return arr
+}
