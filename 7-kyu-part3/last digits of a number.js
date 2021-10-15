fb16588d29d8de0b5675d19c3dfc78933017fdf0
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2021-10-15 21:53:47 +0700

    Create last digits of a number.js

---
+++ 7-kyu-part3/last digits of a number.js
@@ -0,0 +1,24 @@
+/*
+Description:
+Your job is to implement a function which returns the last D digits of an integer N as a list.
+
+Special cases:
+If D > (the number of digits of N), return all the digits.
+If D <= 0, return an empty list.
+Examples:
+N = 1
+D = 1
+result = [1]
+
+N = 1234
+D = 2
+result = [3, 4]
+
+N = 637547
+D = 6
+result = [6, 3, 7, 5, 4, 7]
+*/
+
+function lastDigit(n, d) {
+  return   (d <= 0) ? [] : [...String(n)].slice(-d).map(Number);
+}
