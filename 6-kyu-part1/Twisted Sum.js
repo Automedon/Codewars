733e6e1ff0498bd3e5cc443d0cc991aa4b679d1b
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2021-10-20 08:40:47 +0700

    Create Twisted Sum.js

---
+++ 6-kyu-part1/Twisted Sum.js
@@ -0,0 +1,22 @@
+/*
+Description:
+Find the sum of the digits of all the numbers from 1 to N (both ends included).
+
+Examples
+# N = 4
+1 + 2 + 3 + 4 = 10
+
+# N = 10
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) = 46
+
+# N = 12
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) + (1 + 1) + (1 + 2) = 51
+*/
+
+function twistedSum(n) {
+  let twistedSum = 0
+  for (let i = 0; i<=n; i++){
+    i.toString().split('').map(v=>twistedSum+=v*1)
+  }
+  return twistedSum;
+}
