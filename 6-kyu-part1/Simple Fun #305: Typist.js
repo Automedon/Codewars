4d3630d21ba002788c83257e5a59fe44d0f2930b
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2021-11-01 17:41:27 +0700

    Create Simple Fun #305: Typist.js

---
+++ 6-kyu-part1/Simple Fun #305: Typist.js
@@ -0,0 +1,40 @@
+/*
+Description:
+Task
+John is a typist. He has a habit of typing: he never use the Shift key to switch case, just using only Caps Lock.
+
+Given a string s. Your task is to count how many times the keyboard has been tapped by John.
+
+You can assume that, at the beginning the Caps Lock light is not lit.
+
+Input/Output
+[input] string s
+
+A non-empty string. It contains only English letters(uppercase or lowercase).
+
+1 ≤ s.length ≤ 10000
+
+[output] an integer
+
+The number of times John tapped the keyboard.
+
+Example
+For s = "a", the output should be 1.
+
+John hit button a.
+
+For s = "aa", the output should be 2.
+
+John hit button a, a.
+
+For s = "A", the output should be 2.
+
+John hit button Caps Lock, A.
+
+For s = "Aa", the output should be 4.
+
+John hit button Caps Lock, A, Caps Lock, a.
+
+*/
+
+let typist = s => s.length + s.match(/[A-Z]+|[a-z]+/g).length - (s[0] <= 'Z' ? 0 : 1);
