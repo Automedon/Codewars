730492088e9a647afb329c20b34850eb8938b6e9
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-04-02 11:33:07 +0700

    Create "Center yourself", says the monk..js

---
+++ 7-kyu-part3/"Center yourself", says the monk..js
@@ -0,0 +1,30 @@
+/*
+Description:
+Your company MRE Tech has hired a spiritual consultant who advised on a new Balance policy: Don't take sides, don't favour, stay in the middle. This policy even applies to the software where all strings should now be centered. You are the poor soul to implement it.
+
+Task
+Implement a function center that takes a string strng, an integer width, and an optional character fill (default: ' ') and returns a new string of length width where strng is centered and on the right and left padded with fill.
+
+center(strng, width, fill=' ')
+If the left and right padding cannot be of equal length make the padding on the left side one character longer.
+
+If strng is longer than width return strng unchanged.
+
+Examples
+center('a', 3)  # returns " a "
+center('abc', 10, '_')  # returns "____abc___"
+center('abcdefg', 2)  # returns "abcdefg"
+
+*/
+
+function center (str, width, fill = ' ') {
+  let j = 0;
+  for(let i = str.length;i<width;i++,j++){
+    if(j%2==0){
+      str = fill+str
+    } else {
+      str = str+fill
+    }
+  }
+  return str
+}
