4ae6d5d486fe04a88e96e2b3781e8f3ae0576cd0
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-05-13 13:19:13 +0700

    Update Acrostic reader.js

--- 7-kyu-part3/Acrostic reader.js
+++ 7-kyu-part3/Acrostic reader.js
@@ -1,7 +1,6 @@
 /*
 Description:
 An acrostic is a text in which the first letter of each line spells out a word. It is also a quick and cheap way of writing a poem for somebody, as exemplified below :
-
 Write a program that reads an acrostic to identify the "hidden" word. Specifically, your program will receive a list of words (reprensenting an acrostic) and will need to return a string corresponding to the word that is spelled out by taking the first letter of each word in the acrostic.
 */
 
