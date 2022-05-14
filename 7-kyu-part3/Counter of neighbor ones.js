3a6ae6def1e942bea0151332d649e9ab93ac3b6e
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-05-15 00:13:27 +0700

    Update Counter of neighbor ones.js

--- 7-kyu-part3/Counter of neighbor ones.js
+++ 7-kyu-part3/Counter of neighbor ones.js
@@ -11,19 +11,19 @@ Example
 
 function onesCounter(input) {
   const arr = [];
-  let count = 0
-  for(let i =0;i<input.length;i++){
-    if (input[i]){
-      count+=1
+  let count = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i]) {
+      count += 1;
     } else {
-        if(count){
-          arr.push(count)
-          }
-        count = 0
+      if (count) {
+        arr.push(count);
+      }
+      count = 0;
     }
   }
-  if(count){
-    arr.push(count)
+  if (count) {
+    arr.push(count);
   }
-  return arr
+  return arr;
 }
