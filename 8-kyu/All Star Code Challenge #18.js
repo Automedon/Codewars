fc5989376cb19af7d8dc8a3ff1dfeedcc5f862a9
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-05-24 19:57:55 +0300

    Update All Star Code Challenge #18.js

--- 8-kyu/All Star Code Challenge #18.js
+++ 8-kyu/All Star Code Challenge #18.js
@@ -1,7 +1,6 @@
 /*
 Description:
 This Kata is intended as a small challenge for my students
-
 All Star Code Challenge #18
 
 Create a function called that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
