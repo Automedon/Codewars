7a441d5ff2538148e938f89f145b350a723137a7
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-05-31 17:28:05 +0300

    Update Even and Odd !.js

--- 7-kyu-part3/Even and Odd !.js
+++ 7-kyu-part3/Even and Odd !.js
@@ -1,7 +1,6 @@
 /*
 Description:
 Given a number N, can you fabricate the two numbers NE and NO such that NE is formed by even digits of N and NO is formed by odd digits of N? Examples:
-
 input	NE	NO
 126453	264	153
 3012	2	31
