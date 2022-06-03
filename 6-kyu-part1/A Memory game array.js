7636e48a52680a2a3820976461ae0a99bc801bf4
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-06-03 12:27:00 +0300

    Update A Memory game array.js

--- 6-kyu-part1/A Memory game array.js
+++ 6-kyu-part1/A Memory game array.js
@@ -5,7 +5,6 @@ Memory games basics
 In the memory game, numbers are randomly placed face-down in a grid. Each turn, the player chooses a number to peek at, and then replace, face down. If they choose two of the same number in a row, those numbers are removed from the grid. The aim is to clear the board!
 
 Your job is to produce an array of pairs of numbers, placed in a random spot in the array.
-
 The array (and result) could look somthing like:
 
 [3,1,2,1,3,2,4,4]
