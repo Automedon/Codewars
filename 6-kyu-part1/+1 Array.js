541f7e67174e5b182cf0ef4f28cba921e66ed972
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-06-12 19:13:23 +0300

    Update +1 Array.js

--- 6-kyu-part1/+1 Array.js
+++ 6-kyu-part1/+1 Array.js
@@ -1,7 +1,6 @@
 /*
 Description:
 Given an array of integers of any length, return an array that has 1 added to the value represented by the array.
-
 the array can't be empty
 only non-negative, single digit integers are allowed
 Return nil (or your language's equivalent) for invalid inputs.
