1a77193d222afffeb8cd913464e76c4364acac42
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-06-15 14:23:11 +0300

    Update 1RM Calculator.js

--- 6-kyu-part1/1RM Calculator.js
+++ 6-kyu-part1/1RM Calculator.js
@@ -5,7 +5,6 @@ You just got done with your set at the gym, and you are wondering how much weigh
 Epley
 
 McGlothin
-
 Lombardi
 
 Your function will receive a weight w and a number of repetitions r and must return your projected one repetition maximum. Since you are not sure which formula to use and you are feeling confident, your function will return the largest value from the three formulas shown above, rounded to the nearest integer. However, if the number of repetitions passed in is 1 (i.e., it is already a one rep max), your function must return w. Also, if the number of repetitions passed in is 0 (i.e., no repetitions were completed), your function must return 0.
