b99af4632259dd261c7d7454ed345e1454e52b98
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-06-18 15:30:24 +0300

    Update 1 n- Cycle.js

--- 6-kyu-part1/1 n- Cycle.js
+++ 6-kyu-part1/1 n- Cycle.js
@@ -1,7 +1,6 @@
 /*
 Description:
 Let be n an integer prime with 10 e.g. 7.
-
 1/7 = 0.142857 142857 142857 ....
 
 We see that the decimal part has a cycle: 142857. The length of this cycle is 6. In the same way:
