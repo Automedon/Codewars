25a7fa567c99c8e116bf900cec39693ed682fa07
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-06-20 13:49:09 +0300

    Update A Rule of Divisibility by 13.js

--- 6-kyu-part1/A Rule of Divisibility by 13.js
+++ 6-kyu-part1/A Rule of Divisibility by 13.js
@@ -5,7 +5,6 @@ When you divide the successive powers of 10 by 13 you get the following remainde
 1, 10, 9, 12, 3, 4.
 
 Then the whole pattern repeats.
-
 Hence the following method: Multiply the right most digit of the number with the left most number in the sequence shown above, the second right most digit to the second left most digit of the number in the sequence. The cycle goes on and you sum all these products. Repeat this process until the sequence of sums is stationary.
 
 ...........................................................................
