c3ae30ad97b5e4548c90c9f4542edd925fac0f86
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-06-29 15:12:14 +0300

    Update A tetrahedron of cannonballs.js

--- 6-kyu-part1/A tetrahedron of cannonballs.js
+++ 6-kyu-part1/A tetrahedron of cannonballs.js
@@ -3,7 +3,6 @@ Description:
 Previously on Codewars...
 
 "Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
-
 1st (1)   2nd (3)    3rd (6)
 *          **        ***
            *         **
