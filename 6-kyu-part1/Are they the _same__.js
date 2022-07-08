c0840f290cfee00452f326600f6055ad01935221
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-07-08 10:22:24 +0300

    Update Are they the _same__.js

--- 6-kyu-part1/Are they the _same__.js
+++ 6-kyu-part1/Are they the _same__.js
@@ -1,7 +1,6 @@
 /*
 Description:
 Given two arrays a and b write a function comp(a, b) (compSame(a, b) in Clojure) that checks whether the two arrays have the "same" elements, with the same multiplicities. "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
-
 Examples
 Valid arrays
 a = [121, 144, 19, 161, 19, 144, 19, 11]  
