f1c3561e78389d238e72e2ffbed8a267ae8fb260
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-07-09 19:09:00 +0300

    Update Almost Even.js

--- 6-kyu-part1/Almost Even.js
+++ 6-kyu-part1/Almost Even.js
@@ -1,7 +1,6 @@
 /*
 Description:
 We need the ability to divide an unknown integer into a given number of even parts — or at least as even as they can be. The sum of the parts should be the original value, but each part should be an integer, and they should be as close as possible.
-
 Example code:
 
 splitInteger(20, 6)  //  returns [3, 3, 3, 3, 4, 4]
