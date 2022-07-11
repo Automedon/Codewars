fa0547748bf5504e91025a873bcc2d7346b892dd
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-07-11 15:03:37 +0300

    Update Alphabet war - airstrike - letters massacre.js

--- 6-kyu-part1/Alphabet war - airstrike - letters massacre.js
+++ 6-kyu-part1/Alphabet war - airstrike - letters massacre.js
@@ -3,7 +3,6 @@ Description:
 Introduction
 There is a war and nobody knows - the alphabet war!
 There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began. The letters called airstrike to help them in war - dashes and dots are spreaded everywhere on the battlefield.
-
 Task
 Write a function that accepts fight string consists of only small letters and * which means a bomb drop place. Return who wins the fight after bombs are exploded. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
 
