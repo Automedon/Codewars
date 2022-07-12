ceb8b6678d767a216f8c7c00482bbfde16fe53db
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-07-12 11:10:07 +0300

    Update Alphabetized.js

--- 6-kyu-part1/Alphabetized.js
+++ 6-kyu-part1/Alphabetized.js
@@ -2,7 +2,6 @@
 Description:
 The alphabetized kata
 Re-order the characters of a string, so that they are concatenated into a new string in "case-insensitively-alphabetical-order-of-appearance" order. Whitespace and punctuation shall simply be removed!
-
 The input is restricted to contain no numerals and only words containing the english alphabet letters.
 
 Example:
