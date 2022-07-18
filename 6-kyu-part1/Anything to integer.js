b4804bcc74241d3f88a50455e9684eecf3ed75f7
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-07-18 20:13:11 +0300

    Update Anything to integer.js

--- 6-kyu-part1/Anything to integer.js
+++ 6-kyu-part1/Anything to integer.js
@@ -3,7 +3,6 @@ Description:
 Your task is to program a function which converts any input to an integer.
 
 Do not perform rounding, the fractional part should simply be discarded.
-
 If converting the input to an integer does not make sense (with an object, for instance), the function should return 0 (zero).
 
 Also, Math.floor(), parseInt() and parseFloat() are disabled for your unconvenience.
