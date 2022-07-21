19ea13be8c49446f34c6e5636989c9a8ac447d95
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-07-21 15:15:58 +0300

    Update Array Helpers.js

--- 6-kyu-part1/Array Helpers.js
+++ 6-kyu-part1/Array Helpers.js
@@ -3,7 +3,6 @@ Description:
 This kata is designed to test your ability to extend the functionality of built-in classes. In this case, we want you to extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
 
 Explanation:
-
 square() must return a copy of the array, containing all values squared
 cube() must return a copy of the array, containing all values cubed
 average() must return the average of all array values; on an empty array must return NaN (note: the empty array is not tested in Ruby!)
