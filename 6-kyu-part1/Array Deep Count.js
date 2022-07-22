b35c7118b7598977adb4ed39cf875f328dd655f1
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-07-22 21:21:49 +0300

    Update Array Deep Count.js

--- 6-kyu-part1/Array Deep Count.js
+++ 6-kyu-part1/Array Deep Count.js
@@ -1,7 +1,6 @@
 /*
 Description:
 Array.prototype.length will give you the number of top-level elements in an array.
-
 Your task is to create a function deepCount that returns the number of ALL elements within an array, including any within inner-level arrays.
 
 For example:
