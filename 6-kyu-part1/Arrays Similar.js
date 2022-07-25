2cfa3c78b56d4c0b055894a2ee9dd0307c0565f9
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-07-25 09:33:01 +0300

    Update Arrays Similar.js

--- 6-kyu-part1/Arrays Similar.js
+++ 6-kyu-part1/Arrays Similar.js
@@ -1,7 +1,6 @@
 /*
 Description:
 Write a function that determines whether the passed in arrays are similar. Similar means they contain the same elements, and the same number of occurrences of elements.
-
 var arr1 = [1, 2, 2, 3, 4],
     arr2 = [2, 1, 2, 4, 3],
     arr3 = [1, 2, 3, 4],
