20eeb7495638b5e48af74593157f10ed83da6e92
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-07-28 10:58:01 +0300

    Update Arrh, grabscrab!.js

--- 6-kyu-part1/Arrh, grabscrab!.js
+++ 6-kyu-part1/Arrh, grabscrab!.js
@@ -7,7 +7,6 @@ At long last, we need a way to unscramble what these pirates are saying.
 Write a function that will accept a jumble of letters as well as a dictionary, and output a list of words that the pirate might have meant.
 
 For example:
-
 grabscrab( "ortsp", ["sport", "parrot", "ports", "matey"] )
 Should return ["sport", "ports"].
 
