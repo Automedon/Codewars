c1a7fe5ae5a06874c0c396a98f6e82183e451b6c
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-07-29 12:17:20 +0300

    Update Array.diff.js

--- 6-kyu-part1/Array.diff.js
+++ 6-kyu-part1/Array.diff.js
@@ -1,7 +1,6 @@
 /*
 Description:
 Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
-
 It should remove all values from list a, which are present in list b.
 
 array_diff([1,2],[1]) == [2]
