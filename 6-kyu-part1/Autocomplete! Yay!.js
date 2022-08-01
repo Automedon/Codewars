10a4c10089b75ef30b390d811cf4719ff74f202a
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-08-01 07:21:54 +0300

    Update Autocomplete! Yay!.js

--- 6-kyu-part1/Autocomplete! Yay!.js
+++ 6-kyu-part1/Autocomplete! Yay!.js
@@ -5,7 +5,6 @@ It's time to create an autocomplete function! Yay!
 The autocomplete function will take in an input string and a dictionary array and return the values from the dictionary that start with the input string. If there are more than 5 matches, restrict your output to the first 5 results. If there are no matches, return an empty array.
 
 Example:
-
 autocomplete('ai', ['airplane','airport','apple','ball']) = ['airplane','airport']
 For this kata, the dictionary will always be a valid array of strings. Please return all results in the order given in the dictionary, even if they're not always alphabetical. The search should NOT be case sensitive, but the case of the word should be preserved when it's returned.
 
