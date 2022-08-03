78cf3b2338956366c2a1392b56c49b9fd2fbfb53
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-08-03 07:15:47 +0300

    Update Backspaces in string.js

--- 6-kyu-part1/Backspaces in string.js
+++ 6-kyu-part1/Backspaces in string.js
@@ -3,7 +3,6 @@ Description:
 Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
 
 Your task is to process a string with "#" symbols.
-
 Examples
 "abc#d##c"      ==>  "ac"
 "abc##d######"  ==>  ""
