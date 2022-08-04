d56ee1fcb617e875d5649ac99ab285ed6fae9b6d
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-08-04 10:27:16 +0300

    Update Backwards Read Primes.js

--- 6-kyu-part1/Backwards Read Primes.js
+++ 6-kyu-part1/Backwards Read Primes.js
@@ -1,7 +1,6 @@
 /*
 Description:
 Backwards Read Primes are primes that when read backwards in base 10 (from right to left) are a different prime. (This rules out primes which are palindromes.)
-
 Examples:
 13 17 31 37 71 73 are Backwards Read Primes
 13 is such because it's prime and read from right to left writes 31 which is prime too. Same for the others.
