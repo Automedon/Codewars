2c26b03c617be7ec487bc9ffab57fae0c9f55c7f
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-08-10 12:45:48 +0300

    Update Binary to Text (ASCII) Conversion.js

--- 6-kyu-part1/Binary to Text (ASCII) Conversion.js
+++ 6-kyu-part1/Binary to Text (ASCII) Conversion.js
@@ -5,7 +5,6 @@ Write a function that takes in a binary string and returns the equivalent decode
 Each 8 bits on the binary string represent 1 character on the ASCII table.
 
 The input string will always be a valid binary string.
-
 Characters can be in the range from "00000000" to "11111111" (inclusive)
 
 Note: In the case of an empty binary string your function should return an empty string.
