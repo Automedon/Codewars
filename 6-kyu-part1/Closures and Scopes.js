c9e9475b137f15b2597756f2283e0ad6add7597d
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-08-13 19:05:03 +0300

    Update Closures and Scopes.js

--- 6-kyu-part1/Closures and Scopes.js
+++ 6-kyu-part1/Closures and Scopes.js
@@ -3,7 +3,6 @@ Description:
 We want to create a function, which returns an array of functions, which return their index in the array. For better understanding, here an example:
 
 var callbacks = createFunctions(5); // create an array, containing 5 functions
-
 callbacks[0](); // must return 0
 callbacks[3](); // must return 3
 We already implemented that function, but when we actually run the code, the result doesn't look like what we expected. Can you spot, what's wrong with it? A test fixture is also available
