70e210feb03a6322eb7c383387a4b837e4227974
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-08-16 10:27:04 +0300

    Update Coding Meetup #10 - Higher-Order Functions Series - Create usernames.js

--- 6-kyu-part1/Coding Meetup #10 - Higher-Order Functions Series - Create usernames.js
+++ 6-kyu-part1/Coding Meetup #10 - Higher-Order Functions Series - Create usernames.js
@@ -7,7 +7,6 @@ var list1 = [
   { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure' }
 ];
 write a function that adds the username property to each object in the input array:
-
 [
   { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby', 
     username: 'emilyn1990' },
