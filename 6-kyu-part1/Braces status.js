65401648a43c844fc56171b4abb4a35454826d2a
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-08-18 14:42:45 +0300

    Update Braces status.js

--- 6-kyu-part1/Braces status.js
+++ 6-kyu-part1/Braces status.js
@@ -3,7 +3,6 @@ Description:
 Write a function that checks the braces status in a string, and return True if all braces are properly closed, or False otherwise. Available types of brackets: (), [], {}.
 
 Please note, you need to write this function without using regex!
-
 Examples
 "([[some](){text}here]...)"  =>  true
 "{([])}"                     =>  true
