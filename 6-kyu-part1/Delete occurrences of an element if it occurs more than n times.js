de3a6a67eb62494d7f57997399b69d375b789958
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-08-20 10:07:55 +0300

    Update Delete occurrences of an element if it occurs more than n times.js

--- 6-kyu-part1/Delete occurrences of an element if it occurs more than n times.js
+++ 6-kyu-part1/Delete occurrences of an element if it occurs more than n times.js
@@ -5,7 +5,6 @@ Alice and Bob were on a holiday. Both of them took many pictures of the places t
 
 Task
 Given a list lst and a number N, create a new list that contains each number of lst at most N times without reordering. For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
-
 Example
   deleteNth ([1,1,1,1],2) // return [1,1]
 
