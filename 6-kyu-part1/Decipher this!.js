f24aafa22a099de68e29e5510039df7aed84d60b
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-08-22 09:30:10 +0300

    Update Decipher this!.js

--- 6-kyu-part1/Decipher this!.js
+++ 6-kyu-part1/Decipher this!.js
@@ -3,7 +3,6 @@ Description:
 You are given a secret message you need to decipher. Here are the things you need to know to decipher it:
 
 For each word:
-
 the second and the last letter is switched (e.g. Hello becomes Holle)
 the first letter is replaced by its character code (e.g. H becomes 72)
 Note: there are no special characters used, only letters and spaces
