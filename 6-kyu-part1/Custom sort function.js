4d4ef061a78d12852a329f2f4ed151ff75286ff4
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-08-23 16:05:43 +0300

    Update Custom sort function.js

--- 6-kyu-part1/Custom sort function.js
+++ 6-kyu-part1/Custom sort function.js
@@ -1,7 +1,6 @@
 /*
 Description:
 Complete the sort function so that it returns the items passed into it in alphanumerical order. Conveniently enough, the standard array sort method has been disabled for you so that you are forced to create your own.
-
 Example:
 
 sort([1,3,2]) // should return [1,2,3]
