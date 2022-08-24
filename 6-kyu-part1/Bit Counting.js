14ebbfa14ec57e77f39ae0302ad3c11391ed3189
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-08-24 08:34:23 +0300

    Update Bit Counting.js

--- 6-kyu-part1/Bit Counting.js
+++ 6-kyu-part1/Bit Counting.js
@@ -1,7 +1,6 @@
 /*
 Description:
 Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
-
 Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 */
 
