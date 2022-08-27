43489baff0c5be04d5461ac830f0b67ef6512d6b
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-08-27 16:02:53 +0300

    Update Odd Even number of divisors.js

--- 6-kyu-part1/Odd Even number of divisors.js
+++ 6-kyu-part1/Odd Even number of divisors.js
@@ -7,7 +7,6 @@ Examples:
 All prime numbers have exactly two divisors (hence 'even')
 
 For n=12 the divisors are [1,2,3,4,6,12] – 'even'
-
 For n=4 the divisors are [1,2,4] – 'odd'
 */
 function oddity(n) {
