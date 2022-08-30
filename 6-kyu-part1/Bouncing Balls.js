7a0a9eacbf2d79840f2d1e3d55cdff57f444375e
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-08-30 11:11:21 +0300

    Update Bouncing Balls.js

--- 6-kyu-part1/Bouncing Balls.js
+++ 6-kyu-part1/Bouncing Balls.js
@@ -6,7 +6,6 @@ He drops the ball out of the window. The ball bounces (for example), to two-thir
 His mother looks out of a window 1.5 meters from the ground.
 
 How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
-
 Three conditions must be met for a valid experiment:
 Float parameter "h" in meters must be greater than 0
 Float parameter "bounce" must be greater than 0 and less than 1
