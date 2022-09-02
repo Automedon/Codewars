42bf8bd7b353a609773c3e552cefc8d0aa93dd6c
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-09-02 14:12:29 +0300

    Update Dead Ants.js

--- 6-kyu-part1/Dead Ants.js
+++ 6-kyu-part1/Dead Ants.js
@@ -8,7 +8,6 @@ It looks something like this:
 But suddenly there is a rumour that a dropped chicken sandwich has been spotted on the ground ahead. The ants surge forward! Oh No, it's an ant stampede!!
 
 Some of the slower ants are trampled, and their poor little ant bodies are broken up into scattered bits.
-
 The resulting carnage looks like this:
 
 ...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t
