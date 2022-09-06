b2ea644ac09064fc147f10cf8e6e7ebc5cddbf85
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-09-06 11:20:03 +0300

    Update Count characters in your string.js

--- 6-kyu-part1/Count characters in your string.js
+++ 6-kyu-part1/Count characters in your string.js
@@ -2,7 +2,6 @@
 The main idea is to count all the occuring characters(UTF-8) in string. If you have string like this aba then the result should be { 'a': 2, 'b': 1 }
 
 What if the string is empty ? Then the result should be empty object literal { }
-
 For C#: Use a Dictionary<char, int> for this kata!
 */
 
