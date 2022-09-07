a710e2f8a02ff6ae263c55a78a50332faf714c0f
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-09-07 10:45:54 +0300

    Update Count Repeats.js

--- 6-kyu-part1/Count Repeats.js
+++ 6-kyu-part1/Count Repeats.js
@@ -3,7 +3,6 @@ Description:
 Write a function that returns the count of characters that have to be removed in order to get a string with no consecutive repeats.
 
 Note: This includes any characters
-
 Examples
 'abbbbc'  => 'abc'    #  answer: 3
 'abbcca'  => 'abca'   #  answer: 2
