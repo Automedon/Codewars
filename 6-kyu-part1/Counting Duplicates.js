c0ea640fced5c364db1f6d36e6dac5e1ac1ed15d
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-09-08 08:49:22 +0300

    Update Counting Duplicates.js

--- 6-kyu-part1/Counting Duplicates.js
+++ 6-kyu-part1/Counting Duplicates.js
@@ -4,6 +4,7 @@ Count the number of Duplicates
 Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
 
 Example
+
 "abcde" -> 0 # no characters repeats more than once
 "aabbcde" -> 2 # 'a' and 'b'
 "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
