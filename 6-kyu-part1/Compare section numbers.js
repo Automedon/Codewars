843d404daff5d8e8c0cd933014a97be1d37b1774
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-09-11 14:18:24 +0300

    Update Compare section numbers.js

--- 6-kyu-part1/Compare section numbers.js
+++ 6-kyu-part1/Compare section numbers.js
@@ -3,7 +3,6 @@ Description:
 Section numbers are strings of dot-separated integers. The highest level sections (chapters) are numbered 1, 2, 3, etc. Second level sections are numbered 1.1, 1.2, 1.3, 2.1, 2.2, 2.3, etc. Next level sections are numbered 1.1.1, 1.1.2, 1.1.2, 1.2.1, 1.2.2, erc. There is no bound on the number of sections a document may have, nor is there any bound on the number of levels.
 
 A section of a certain level may appear directly inside a section several levels higher without the levels between. For example, section 1.0.1 may appear directly under section 1, without there being any level 2 section. Section 1.1 comes after section 1.0.1. Sections with trailing ".0" are considered to be the same as the section with the trailing ".0" truncated. Thus, section 1.0 is the same as section 1, and section 1.2.0.0 is the same as section 1.2.
-
 Write a function cmp(section1, section2) that returns -1, 0, or 1 depending on whether section1 is before, same as, or after section2 respectively.
 */
 function cmp(section1, section2) {
