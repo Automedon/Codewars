37e7bb3eda514a597b6399685b79faa90fb49471
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-09-19 08:47:18 +0300

    Update Buying a car.js

--- 6-kyu-part1/Buying a car.js
+++ 6-kyu-part1/Buying a car.js
@@ -7,7 +7,6 @@ A man has a rather old car being worth $2000. He saw a secondhand car being wort
 He thinks he can save $1000 each month but the prices of his old car and of the new one decrease of 1.5 percent per month. Furthermore this percent of loss increases by 0.5 percent at the end of every two months. Our man finds it difficult to make all these calculations.
 
 Can you help him?
-
 How many months will it take him to save up enough money to buy the car he wants, and how much money will he have left over?
 
 Parameters and return of function:
