ecedee2ed63f408e2b13fac14fcdcc7db443b8ab
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-09-20 10:50:50 +0300

    Update Build Tower.js

--- 6-kyu-part1/Build Tower.js
+++ 6-kyu-part1/Build Tower.js
@@ -5,7 +5,6 @@ Build Tower by the following given argument:
 number of floors (integer and always greater than 0).
 
 Tower block is represented as *
-
 Python: return a list;
 JavaScript: returns an Array;
 C#: returns a string[];
