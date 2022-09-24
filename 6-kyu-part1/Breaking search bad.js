77273c9ff58dcee2dfdcfce05e3187416619a493
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-09-24 17:50:51 +0300

    Update Breaking search bad.js

--- 6-kyu-part1/Breaking search bad.js
+++ 6-kyu-part1/Breaking search bad.js
@@ -5,7 +5,6 @@ We have a search function who return an array of titles that match the String pa
 var titles = ['Rocky 1', 'Rocky 2', 'My Little Poney'];
 search('ock')  // ==> should return ['Rocky 1', 'Rocky 2']
 But the function return some weird result and skip some of the matching results.
-
 Does the function have special movie taste ?
 
 Let's figure out !
