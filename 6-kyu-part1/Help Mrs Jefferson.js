af1a63d734169ed24f11e1f260002a6e375d18ed
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-09-26 11:27:59 +0300

    Update Help Mrs Jefferson.js

--- 6-kyu-part1/Help Mrs Jefferson.js
+++ 6-kyu-part1/Help Mrs Jefferson.js
@@ -3,7 +3,6 @@ Description:
 Mrs Jefferson is a great teacher. One of her strategies that helped her to reach astonishing results in the learning process is to have some fun with her students. At school, she wants to make an arrangement of her class to play a certain game with her pupils. For that, she needs to create the arrangement with the minimum amount of groups that have consecutive sizes.
 
 Let's see. She has 14 students. After trying a bit she could do the needed arrangement: [5, 4, 3, 2]
-
 one group of 5 students
 another group of 4 students
 then, another one of 3
