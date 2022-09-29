e02fa8bec1c6129a801f19e2e3fe8095c562e7d5
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-09-29 10:02:17 +0300

    Update CamelCase Method.js

--- 6-kyu-part1/CamelCase Method.js
+++ 6-kyu-part1/CamelCase Method.js
@@ -3,7 +3,6 @@ Description:
 Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
 
 For instance:
-
 "hello case".camelCase() => HelloCase
 "camel case word".camelCase() => CamelCaseWord
 Don't forget to rate this kata! Thanks :)
