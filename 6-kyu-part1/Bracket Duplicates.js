5bd5186aa8e7aac02debd71c1da8a8444fb6c781
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-10-06 20:30:24 +0300

    Update Bracket Duplicates.js

--- 6-kyu-part1/Bracket Duplicates.js
+++ 6-kyu-part1/Bracket Duplicates.js
@@ -3,7 +3,6 @@ Description:
 Create a program that will take in a string as input and, if there are duplicates of more than two alphabetical characters in the string, returns the string with all the extra characters in a bracket.
 
 For example, the input "aaaabbcdefffffffg" should return "aa[aa]bbcdeff[fffff]g"
-
 Please also ensure that the input is a string, and return "Please enter a valid string" if it is not.
 */
 function stringParse(string) {
