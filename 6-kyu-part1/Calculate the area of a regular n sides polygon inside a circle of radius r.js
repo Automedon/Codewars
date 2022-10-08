e9fb55086b4b4a90a632707651e72f7e432d6e02
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-10-08 15:24:53 +0300

    Update Calculate the area of a regular n sides polygon inside a circle of radius r.js

--- 6-kyu-part1/Calculate the area of a regular n sides polygon inside a circle of radius r.js
+++ 6-kyu-part1/Calculate the area of a regular n sides polygon inside a circle of radius r.js
@@ -6,7 +6,6 @@ function areaOfPolygonInsideCircle(circleRadius, numberOfSides)
 It should calculate the area of a regular polygon of numberOfSides or number_of_sides sides inside a circle of radius circleRadius or circle_radius which passes through all the vertices of the polygon (such circle is called circumscribed circle or circumcircle). The answer should be a number rounded to 3 decimal places.
 
 Input :: Output Examples
-
 areaOfPolygonInsideCircle(3, 3) // returns 11.691
 
 areaOfPolygonInsideCircle(5.8, 7) // returns 92.053
