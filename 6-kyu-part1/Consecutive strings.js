b25323a5d96ba50587912ea7fdb3f13e2084e588
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-10-09 19:53:28 +0300

    Update Consecutive strings.js

--- 6-kyu-part1/Consecutive strings.js
+++ 6-kyu-part1/Consecutive strings.js
@@ -2,7 +2,6 @@
 You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
 
 #Example: longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
-
 n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
 */
 
