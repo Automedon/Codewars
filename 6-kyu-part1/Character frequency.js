c158f04a749f5d1a4b2a7bb044c1c68472ce1be2
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-10-13 09:33:21 +0300

    Update Character frequency.js

--- 6-kyu-part1/Character frequency.js
+++ 6-kyu-part1/Character frequency.js
@@ -3,7 +3,6 @@ Description:
 Write a function that takes a piece of text in the form of a string and returns the letter frequency count for the text. This count excludes numbers, spaces and all punctuation marks. Upper and lower case versions of a character are equivalent and the result should all be in lowercase.
 
 The function should return a list of tuples (in Python) or arrays (in other languages) sorted by the most frequent letters first. The Rust implementation should return an ordered BTreeMap. Letters with the same frequency are ordered alphabetically. For example:
-
   letterFrequency('aaAabb dddDD hhcc')
 will return
 
