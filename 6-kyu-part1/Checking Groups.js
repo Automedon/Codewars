526d5b558654a8c7165419aa2ba99f712c2cc7c3
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-10-18 07:27:43 +0300

    Update Checking Groups.js

--- 6-kyu-part1/Checking Groups.js
+++ 6-kyu-part1/Checking Groups.js
@@ -1,7 +1,6 @@
 /*
 Description:
 In English and programming, groups can be made using symbols such as () and {} that change meaning. However, these groups must be closed in the correct order to maintain correct syntax.
-
 Your job in this kata will be to make a program that checks a string for correct grouping. For instance, the following groups are done correctly:
 
 ({})
