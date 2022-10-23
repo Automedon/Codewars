d31f8ab09bb582630ee846ed261bfc8768a0e4fc
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-10-23 17:32:34 +0300

    Update Image host filename generator.js

--- 6-kyu-part1/Image host filename generator.js
+++ 6-kyu-part1/Image host filename generator.js
@@ -5,7 +5,6 @@ You are developing an image hosting website.
 You have to create a function for generating random and unique image filenames.
 
 Create a function for generating a random 6 character string which will be used to access the photo URL.
-
 To make sure the name is not already in use, you are given access to an PhotoManager object.
 
 You can call it like so to make sure the name is unique
