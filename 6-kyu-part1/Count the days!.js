4ab48e5e1f0a37ee9a1b2375171ffcef0c7cf8ce
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-10-25 10:26:15 +0300

    Update Count the days!.js

--- 6-kyu-part1/Count the days!.js
+++ 6-kyu-part1/Count the days!.js
@@ -3,7 +3,6 @@ Description:
 Little Annie is very excited for upcoming events. She want's to know how many days she have to wait for a specific event.
 
 Your job is to help her out.
-
 Task: Write a function which returns the number of days from today till the given date. The function will take a Date object as parameter. You have to round the amount of days.
 
 If the event is in the past, return "The day is in the past!"
