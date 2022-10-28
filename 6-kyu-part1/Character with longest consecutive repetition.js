3e3c17f9459afdafab79b1e24fc7228fd49489c0
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-10-28 11:30:31 +0300

    Update Character with longest consecutive repetition.js

--- 6-kyu-part1/Character with longest consecutive repetition.js
+++ 6-kyu-part1/Character with longest consecutive repetition.js
@@ -1,7 +1,6 @@
 /*
 Description:
 For a given string s find the character c with longest consecutive repetition and return a tuple (c, l) (in Haskell Just (Char, Int), in C# Tuple<char?, int>, in Shell a String of comma-separated values c,l, in JavaScript [c,l], in Ruby [c, l], in Groovy [c, l]) where l is the length of the repetition. If there are two or more characters with the same l return the first.
-
 For empty string return ('', 0) (in Haskell Nothing, in C# Tuple<char, int>(null, 0), in Shell ,0, in JavaScript ["",0], in Ruby ["", 0], in Groovy ["", 0]).
 
 Happy coding! :)
