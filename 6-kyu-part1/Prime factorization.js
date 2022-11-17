6815d626816a57f01d45a67c557ca77337ce8f05
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-11-17 07:01:49 +0300

    Update Prime factorization.js

--- 6-kyu-part1/Prime factorization.js
+++ 6-kyu-part1/Prime factorization.js
@@ -8,6 +8,7 @@ Write a class called PrimeFactorizer whose constructor accepts exactly 1 number 
 
 new PrimeFactorizer(24).factor //should return { '2': 3, '3': 1 }
 */
+
 function PrimeFactorizer(n) {
   let i,
     factors = {};
