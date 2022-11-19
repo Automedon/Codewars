fec4c1b7d612444061d2c82603c7f6d1ec4c6ffc
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-11-19 13:03:43 +0300

    Update Highest Scoring Word.js

--- 6-kyu-part1/Highest Scoring Word.js
+++ 6-kyu-part1/Highest Scoring Word.js
@@ -1,7 +1,6 @@
 /*
 Description:
 Given a string of words, you need to find the highest scoring word.
-
 Each letter of a word scores points according to it's position in the alphabet: a = 1, b = 2, c = 3 etc.
 
 You need to return the highest scoring word as a string.
