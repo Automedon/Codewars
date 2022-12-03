407bb4ba62095329d9e5e6ade4b62a1a3669089a
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-12-03 14:26:26 +0300

    Update Hanoi record.js

--- 6-kyu-part1/Hanoi record.js
+++ 6-kyu-part1/Hanoi record.js
@@ -12,6 +12,7 @@ Only one disk can be moved at a time.
 Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack i.e. a disk can only be moved if it is the uppermost disk on a stack.
 No disk may be placed on top of a smaller disk.
 */
+
 var hanoi = function(disks) {
   return 2 ** disks - 1;
 };
