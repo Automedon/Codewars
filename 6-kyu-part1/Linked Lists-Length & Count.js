b099d67e7c16887f6433b737e245dc5680bde860
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-12-04 13:40:44 +0300

    Update Linked Lists-Length & Count.js

--- 6-kyu-part1/Linked Lists-Length & Count.js
+++ 6-kyu-part1/Linked Lists-Length & Count.js
@@ -7,7 +7,6 @@ Implement Length() to count the number of nodes in a linked list.
 length(null) => 0
 length(1 -> 2 -> 3 -> null) => 3
 Implement Count() to count the occurrences of an integer in a linked list.
-
 count(null, 1) => 0
 count(1 -> 2 -> 3 -> null, 1) => 1
 count(1 -> 1 -> 1 -> 2 -> 2 -> 2 -> 2 -> 3 -> 3 -> null, 2) => 4
