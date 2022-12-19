e7a78fd301c209c08e6be018f183ed6bd285152b
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-12-19 16:18:07 +0300

    Update Kebabize.js

--- 6-kyu-part1/Kebabize.js
+++ 6-kyu-part1/Kebabize.js
@@ -5,7 +5,6 @@ Modify the kebabize function so that it converts a camel case string into a keba
 kebabize('camelsHaveThreeHumps') // camels-have-three-humps
 kebabize('camelsHave3Humps') // camels-have-humps
 Notes:
-
 the returned string should only contain lowercase letters
 */
 
