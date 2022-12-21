c0944d400246fdb358badb53aa0fc4882d83ccac
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-12-21 18:14:33 +0300

    Update Multiples of 3 or 5.js

--- 6-kyu-part1/Multiples of 3 or 5.js
+++ 6-kyu-part1/Multiples of 3 or 5.js
@@ -5,7 +5,6 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 
 Note: If the number is a multiple of both 3 and 5, only count it once.
-
 Courtesy of ProjectEuler.net
 */
 
