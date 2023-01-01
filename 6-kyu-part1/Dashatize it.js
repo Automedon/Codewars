a51ce977834b3939d2caebc20713b348aa0c35e0
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-01-01 16:12:00 +0300

    Update Dashatize it.js

--- 6-kyu-part1/Dashatize it.js
+++ 6-kyu-part1/Dashatize it.js
@@ -3,7 +3,6 @@ Description:
 Given a number, return a string with dash'-'marks before and after each odd integer, but do not begin or end the string with a dash mark.
 
 Ex:
-
 dashatize(274) -> '2-7-4'
 dashatize(6815) -> '68-1-5'
 */
