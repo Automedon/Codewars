9c35a9ded251aebd548f9bdd49fff68e5454065f
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-01-04 13:14:56 +0300

    Update Function Composition (2).js

--- 6-kyu-part1/Function Composition (2).js
+++ 6-kyu-part1/Function Composition (2).js
@@ -6,7 +6,6 @@ f3 = compose( f1 f2 )
    Is equivalent to...
 f3(a) = f1( f2( a ) )
 Your task is to create a compose function to carry out this task, which will be passed two functions or lambdas. Ruby functions will be passed, and should return, either a proc or a lambda. Remember that the resulting composed function may be passed multiple arguments!
-
 compose(f , g)(x)
 => f( g( x ) )
 This kata is not available in haskell; that would be too easy!
