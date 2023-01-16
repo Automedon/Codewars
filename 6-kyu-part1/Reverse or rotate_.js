9f24a04b45ab7fef2a98ce6020aa1e789aac22f8
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-01-16 19:58:21 +0300

    Update Reverse or rotate_.js

--- 6-kyu-part1/Reverse or rotate_.js
+++ 6-kyu-part1/Reverse or rotate_.js
@@ -5,7 +5,6 @@ The input is a string str of digits. Cut the string into chunks (a chunk here is
 If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, reverse that chunk; otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
 
 If
-
 sz is <= 0 or if str is empty return ""
 sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
 Examples:
