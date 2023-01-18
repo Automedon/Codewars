8695ef7e83cfac860b24d1c77d573d7e45566504
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-01-18 14:41:05 +0300

    Update Creating a string for an array of objects from a set of words.js

--- 6-kyu-part1/Creating a string for an array of objects from a set of words.js
+++ 6-kyu-part1/Creating a string for an array of objects from a set of words.js
@@ -12,6 +12,7 @@ Doing this manually is a pain. So you've decided to write a short function that 
 
 As a result you'll have a string you may just copy-paste whenever you feel like defining a list of objects - now without the need to put in names, IDs, curly brackets, colon signs, screw up everything, fail searching for a typo and begin anew. This might come in handy with large lists.
 */
+
 function wordsToObject(input) {
   let arr = [];
   input = input.split` `;
