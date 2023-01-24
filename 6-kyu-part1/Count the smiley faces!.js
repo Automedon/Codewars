bdab4f693cf5c1bde639cb486ca52e6f60e3cc9c
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-01-24 11:19:32 +0300

    Update Count the smiley faces!.js

--- 6-kyu-part1/Count the smiley faces!.js
+++ 6-kyu-part1/Count the smiley faces!.js
@@ -13,7 +13,6 @@ Invalid smiley faces:
 ;( :> :} :] 
 
 Example cases:
-
 countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
 countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
 countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
