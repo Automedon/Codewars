ab8438b2f7f318a512a209a7c180fb278059188b
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-01-25 18:13:14 +0300

    Update Count the divisible numbers.js

--- 6-kyu-part1/Count the divisible numbers.js
+++ 6-kyu-part1/Count the divisible numbers.js
@@ -6,6 +6,7 @@ For example:
 x = 6, y = 11 and k = 2, your function should return 3, because there are three numbers divisible by 2 within the range [6..11], namely 6, 8 and 10.
 Note: x<=y
 */
+
 function divisibleCount(x, y, k) {
   return Math.floor(y / k) - Math.floor((x - 1) / k);
 }
