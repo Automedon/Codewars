b6b72350d5037e6541cef5969f7f488dd2980ca9
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-01-30 20:22:24 +0300

    Update Detect Pangram.js

--- 6-kyu-part1/Detect Pangram.js
+++ 6-kyu-part1/Detect Pangram.js
@@ -1,7 +1,6 @@
 /*
 Description:
 A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
-
 Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 */
 
