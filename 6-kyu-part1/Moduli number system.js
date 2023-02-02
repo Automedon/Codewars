e9b6245c5b9c802941833aa0cfb69d4962cb5bd1
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-02-02 11:29:19 +0300

    Update Moduli number system.js

--- 6-kyu-part1/Moduli number system.js
+++ 6-kyu-part1/Moduli number system.js
@@ -20,7 +20,6 @@ Examples: (you can add them in the "Sample tests")
 fromNb2Str(11, [2,3,5]) -> "-1--2--1-"
 
 fromNb2Str(6, [2, 3, 4]) -> "Not applicable", since 2 and 4 are not coprime
-
 fromNb2Str(7, [2, 3]) -> "Not applicable" since 2 * 3 < 7
 */
 function fromNb2Str(n, sys) {
