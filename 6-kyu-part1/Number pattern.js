5f5d9dd15498529dd72717d74c3f34da852d2ace
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-02-21 21:50:18 +0300

    Update Number pattern.js

--- 6-kyu-part1/Number pattern.js
+++ 6-kyu-part1/Number pattern.js
@@ -8,6 +8,7 @@ Good luck!
 
 P.S. If you like this kata, please remember to mark it as ready and assess the rank. P.P.S. If you have any questions or criticisms please list them in the comments section.
 */
+
 var answer = function() {
   return 529;
 };
