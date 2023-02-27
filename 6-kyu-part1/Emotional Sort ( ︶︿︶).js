ea6dc593ac67d24c7d8aa2aff9a80d2292eaf754
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-02-27 19:30:04 +0300

    Update Emotional Sort ( ︶︿︶).js

--- 6-kyu-part1/Emotional Sort ( ︶︿︶).js
+++ 6-kyu-part1/Emotional Sort ( ︶︿︶).js
@@ -13,7 +13,6 @@ Example of the array:[ 'T_T', ':D', ':|', ':)', ':(' ]
 And the second parameter is called "order", if this parameter is true then the order of the emotions will be descending (from Super Happy to Super Sad) if it's false then it will be ascending (from Super Sad to Super Happy)
 
 Example if order is true with the above array: [ ':D', ':)', ':|', ':(', 'T_T' ]
-
 Super Happy -> Happy -> Normal -> Sad -> Super Sad
 If order is false: [ 'T_T', ':(', ':|', ':)', ':D' ]
 
