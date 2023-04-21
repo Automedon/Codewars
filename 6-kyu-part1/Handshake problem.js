cf938795382af0469ae6d8444ceb5f02c2643e9a
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-04-21 07:28:00 +0300

    Update Handshake problem.js

--- 6-kyu-part1/Handshake problem.js
+++ 6-kyu-part1/Handshake problem.js
@@ -7,7 +7,6 @@ However, Johnny would like to know the minimal amount of people that participate
 
 Help Johnny by writing a function, that takes the amount of handshakes and returns the minimal amount of people needed to perform these handshakes (a pair of farmers handshake only once).
 */
-
 function getParticipants(h) {
   for (var i = 0, k = 1; i < h; i += k++);
   return k;
