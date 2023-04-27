d0b8ba63396e4b921ad941d3368c56186204ff59
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-04-27 14:28:14 +0300

    Update Duplicate Encoder.js

--- 6-kyu-part1/Duplicate Encoder.js
+++ 6-kyu-part1/Duplicate Encoder.js
@@ -17,7 +17,6 @@ Notes:
 
 Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is actually the expected result, not the input! (these languages are locked so that's not possible to correct it).
 
-
 */
 
 function duplicateEncode(word) {
