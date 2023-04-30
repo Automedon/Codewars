2ba8fc8c2daaf78fd794229bd48f5c727af69719
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-04-30 20:55:42 +0300

    Update Making Change.js

--- 6-kyu-part1/Making Change.js
+++ 6-kyu-part1/Making Change.js
@@ -27,6 +27,7 @@ Currency.make_change(43) --> %{:Q=>1, :D=>1, :N=>1, :P=>3}
 Currency.make_change(91) --> %{:H=>1, :Q=>1, :D=>1, :N=>1, :P=>1}
 If you liked this kata, check out Part 2.
 */
+
 const makeChange = amount => {
   const change = {};
   while (amount >= 50) {
