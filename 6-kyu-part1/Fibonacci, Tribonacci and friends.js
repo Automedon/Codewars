86cd0613645969e2dcc2db9e4d8a20b126f29b4e
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-05-16 19:32:44 +0300

    Update Fibonacci, Tribonacci and friends.js

--- 6-kyu-part1/Fibonacci, Tribonacci and friends.js
+++ 6-kyu-part1/Fibonacci, Tribonacci and friends.js
@@ -11,7 +11,6 @@ xbonacci {0,0,0,0,1} 10 = {0,0,0,0,1,1,2,4,8,16}
 xbonacci {1,0,0,0,0,0,1} 10 = {1,0,0,0,0,0,1,2,3,6}
 xbonacci {1,1} produces the Fibonacci sequence
 */
-
 function Xbonacci(s, n) {
   if (s.length > n) return s.slice(0, n);
   let sum = [...s];
