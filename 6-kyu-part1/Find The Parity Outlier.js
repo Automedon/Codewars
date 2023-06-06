d6b66d917a631dad7bdffebebc3ad46f94e73055
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2023-06-06 09:36:19 +0300

    Update Find The Parity Outlier.js

--- 6-kyu-part1/Find The Parity Outlier.js
+++ 6-kyu-part1/Find The Parity Outlier.js
@@ -9,7 +9,6 @@ Should return: 11 (the only odd number)
 [160, 3, 1719, 19, 11, 13, -21]
 Should return: 160 (the only even number)
 */
-
 function findOutlier(int) {
   var even = int.filter(a => a % 2 == 0);
   var odd = int.filter(a => a % 2 !== 0);
